f58cf43a083e893e7d97021b53d0ec91587fd47c
Dye <anthony.dye@ies-co.com>
2022-05-10 16:00:07 -0500

    Complete Problem One

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -42,8 +42,12 @@ namespace DatabaseFirstLINQ
         {
             // Write a LINQ query that returns the number of users in the Users table.
             // HINT: .ToList().Count
+            var userNumber = _context.Users.ToList().Count;
+            Console.WriteLine(userNumber);
 
         }
+
+
 
         private void ProblemTwo()
         {
